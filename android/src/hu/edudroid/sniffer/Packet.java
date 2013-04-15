0837b15d0800c0eb697d16a1b183d3477d09d862
Mate Peter <matepeter90@gmail.com>
2013-04-15 17:43:49 +0200

    Added IPChecksum and UDPChecksum to Packet class

--- android/src/hu/edudroid/sniffer/Packet.java
+++ android/src/hu/edudroid/sniffer/Packet.java
@@ -82,6 +82,67 @@ public class Packet {
 		return ret;
 	}
 	
+	public static int IPChecksum(byte[] header) {
+		int sum = 0;
+		int length = header.length;
+		int i = 0;
+		while (length > 1) {
+			// Calculating the sum of byte pairs
+			sum += (((header[i] << 8) & 0xFF00) | ((header[i + 1]) & 0xFF));
+			i += 2;
+			length -= 2;
+		}
+
+		// if length is odd, padd with 0's from right
+		if (length > 0) {
+			sum += (header[i] << 8 & 0xFF00);
+		}
+		int carry = (0xFFFF0000 & sum) >> 16; // Carry
+		if (carry > 0) {
+			sum = sum & 0xFFFF;
+			sum += carry;
+		}
+		return (~sum) & 0xFFFF;
+	}
+	
+	public int UDPchecksum(byte[] ret) {
+		int sum = 0;
+		int length = ret.length;
+		int i = 0;
+		int carry = 0;
+		
+		//Pseudo header
+		sum += (((sourceIp[0] << 8) & 0xFF00) | ((sourceIp[1]) & 0xFF))
+		sum += (((sourceIp[2] << 8) & 0xFF00) | ((sourceIp[3]) & 0xFF))
+		sum += (((destIp[0] << 8) & 0xFF00) | ((destIp[1]) & 0xFF))
+		sum += (((destIp[2] << 8) & 0xFF00) | ((destIp[3]) & 0xFF))
+		sum += TCPIPUtils.toTwoBytes(UDP);
+		sum += TCPIPUtils.toTwoBytes(ret.length);
+		carry = (0xFFFF0000 & sum) >> 16; //Carry
+	    if(carry > 0){
+	    	sum = sum & 0xFFFF;
+	    	sum += carry;
+	    }
+		
+		while(length > 1){
+	    	//Calculating the sum of byte pairs
+	    	sum += (((ret[i] << 8) & 0xFF00) | ((ret[i+1]) & 0xFF));
+	    	i += 2;
+	    	length -= 2;
+	    }
+		//if length is odd, padd with 0's from right
+		if(length > 0){
+			sum += (ret[i] << 8 & 0xFF00);
+		}
+		carry = (0xFFFF0000 & sum) >> 16; //Carry
+	    if(carry > 0){
+	    	sum = sum & 0xFFFF;
+	    	sum += carry;
+	    }
+	    
+	    return (~sum) & 0xFFFF;
+	}
+	
 	public byte[] toByteArray() {
 		byte[] ret = new byte[packetLength];
 		ret[0] = 69;// TCPIPUtils.toByte(version, ihl);
@@ -92,16 +153,20 @@ public class Packet {
 		ret[7] = 0; // Flags, Fragment offset part 2
 		ret[8] = 64; // TTL
 		ret[9] = protocol;
-		System.arraycopy(TCPIPUtils.toTwoBytes(0), 0, ret, 4, 2); // Header checksum TODO
+		System.arraycopy(TCPIPUtils.toTwoBytes(0), 0, ret, 4, 2); // 0's for Header checksum calculation
 		System.arraycopy(sourceIp, 0, ret, 12, 2);
 		System.arraycopy(destIp, 0, ret, 12, 4);
+		byte[] header = new byte[ipHeaderLength];
+		System.arraycopy(ret,0,header,0,ipHeaderLength);
+		System.arraycopy(TCPIPUtils.toTwoBytes(IPChecksum(header)), 0, ret, 4, 2); // Header checksum
 		// No options
 		if (protocol == UDP) {
 			System.arraycopy(TCPIPUtils.toTwoBytes(sourcePort), 0, ret, 20, 2); // Source port
 			System.arraycopy(TCPIPUtils.toTwoBytes(destPort), 0, ret, 22, 2); // Destination port
 			System.arraycopy(TCPIPUtils.toTwoBytes(dataLength + 8), 0, ret, 24, 2); // Data + header length
-			System.arraycopy(TCPIPUtils.toTwoBytes(0), 0, ret, 26, 2); // UDP checksum TODO
+			System.arraycopy(TCPIPUtils.toTwoBytes(0), 0, ret, 26, 2); // 0's for UDP checksum calculation
 			System.arraycopy(data, 0, ret, 28, dataLength);
+			System.arraycopy(TCPIPUtils.toTwoBytes(UDPChecksum(ret)), 0, ret, 26, 2); // UDP checksum
 		}
 		return ret;
 	}
